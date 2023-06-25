67ed3636cfc58dcd7315c299d4b7a2d4640091de
Igor Zyktin <nicord@yandex.ru>
2023-06-25 13:29:12 +0300

    doing

--- minimus/__main__.py
+++ minimus/__main__.py
@@ -1,75 +1,44 @@
-# -*- coding: utf-8 -*-
-
 """Основной модуль.
 """
 
 import time
 
-import click
-from colorama import init
-
 from minimus import output
+from minimus import parse
 from minimus import render
 from minimus import storage
-from minimus import parse
-
-init(autoreset=True)
 
 
-@click.command()
-@click.option('--source',
-              required=True,
-              help='Каталог с исходными материалами')
-@click.option('--target',
-              required=True,
-              help='Каталог для сохранения обработанных материалов')
-@click.option('--force-notes/--no-force-notes',
-              default=False,
-              help='Принудительно перезаписывать '
-                   'файлы заметок даже если они не обновлялись')
-@click.option('--force-media/--no-force-media',
-              default=False,
-              help='Принудительно перезаписывать '
-                   'файлы данных даже если они не обновлялись')
-def main(source: str, target: str, force_notes: bool, force_media: bool):
+def main() -> None:
     """Точка входа.
     """
-    start_time = time.monotonic()
-    output.greeting_message()
+    start_time = time.perf_counter()
 
-    source, target = storage.validate_setup(source, target)
-    output.setup(source, target, force_notes, force_media)
+    output.greet()
+    path = storage.get_path()
+    output.print_path(path)
 
-    cache = storage.gather_cache(source)
+    # cache = storage.gather_cache(source)
 
-    pointers = storage.gather_pointers(source)
-    pointers = storage.skip_private_files(pointers)
-    notes, media = storage.split_pointers(pointers)
+    # pointers = storage.gather_pointers(source)
+    # pointers = storage.skip_private_files(pointers)
+    # notes, media = storage.split_pointers(pointers)
 
     output.header('Сохранение заметок')
-    notes_with_text = storage.load_text(notes)
-    documents = parse.make_documents(notes_with_text)
-    correspondence = render.make_correspondence(documents)
-    render.update_all_documents(documents)
-    storage.save_documents(target, documents, cache, force_notes)
+    # notes_with_text = storage.load_text(notes)
+    # documents = parse.make_documents(notes_with_text)
+    # correspondence = render.make_correspondence(documents)
+    # render.update_all_documents(documents)
+    # storage.save_documents(target, documents, cache, force_notes)
 
     output.header('Генерация вспомогательных файлов')
     tags = render.make_tags(correspondence)
-    storage.save_tags(target, tags)
+    storage.save_tags(path, tags)
     readme = render.make_readme(documents)
-    storage.save_readme(target, readme)
+    storage.save_readme(path, readme)
 
-    output.header('Сохранение прочих файлов')
-    storage.copy_media(target, media, cache, force_media)
-    storage.save_cache(source, cache)
-
-    warnings = parse.extract_warnings(documents)
-    if warnings:
-        output.header('К исходникам есть замечания')
-        output.warnings(warnings)
-
-    output.final_message(time.monotonic() - start_time)
+    output.complete(time.perf_counter() - start_time)
 
 
 if __name__ == '__main__':
-    main()  # pragma: no cover
+    main()
--- minimus/output.py
+++ minimus/output.py
@@ -1,54 +1,44 @@
-# -*- coding: utf-8 -*-
-
 """Модуль для вывода сообщений на экран.
 """
 
-from colorama import Fore
+from pathlib import Path
 
-from minimus import constants
+LOGO = """
+███╗   ███╗██╗███╗   ██╗██╗   ██╗███╗   ███╗██╗   ██╗███████╗    ██████╗     ██████╗
+████╗ ████║██║████╗  ██║██║   ██║████╗ ████║██║   ██║██╔════╝    ╚════██╗   ██╔═████╗
+██╔████╔██║██║██╔██╗ ██║██║   ██║██╔████╔██║██║   ██║███████╗     █████╔╝   ██║██╔██║
+██║╚██╔╝██║██║██║╚██╗██║██║   ██║██║╚██╔╝██║██║   ██║╚════██║    ██╔═══╝    ████╔╝██║
+██║ ╚═╝ ██║██║██║ ╚████║╚██████╔╝██║ ╚═╝ ██║╚██████╔╝███████║    ███████╗██╗╚██████╔╝
+╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝ ╚═════╝ ╚══════╝    ╚══════╝╚═╝ ╚═════╝
+""".strip()  # noqa
+
+LINE = '-' * (max(len(x) for x in LOGO.split('\n')))
 
 
-def greeting_message() -> None:
+def greet() -> None:
     """Распечатать приветствие."""
-    separation_line()
-    print(Fore.LIGHTRED_EX + constants.LOGO)
-    print(Fore.LIGHTRED_EX + f'Version: {constants.__version__}')
-    separation_line()
+    print_line()
+    print(LOGO)
+    print_line()
 
 
-def setup(source: str, target: str,
-          force_notes: bool, force_media: bool) -> None:
-    """Вывести на экран стартовые настройки скрипта."""
-    print(f' Исходники: {source}')
-    print(f'Результаты: {target}')
-
-    if force_notes:
-        print(f'Включена принудительная перезапись файлов с заметками')
-
-    if force_media:
-        print(f'Включена принудительная перезапись вспомогательных файлов')
-
-
-def separation_line() -> None:
+def print_line() -> None:
     """Вывести на экран горизонтальную линию."""
-    print(Fore.LIGHTRED_EX + constants.LINE)
+    print(LINE)
+
+
+def print_path(path: Path) -> None:
+    """Вывести на экран стартовые настройки скрипта."""
+    print(f'Исходный каталог: {path.absolute()}')
 
 
 def header(text: str) -> None:
     """Вывести новый блок текста."""
     print()
-    print(Fore.CYAN + text)
+    print(text)
 
 
-def final_message(seconds: float) -> None:
+def complete(seconds: float) -> None:
     """Вывести на экран сообщение об окончании работы программы."""
-    separation_line()
-    print(Fore.LIGHTRED_EX + f'Обработка заняла {seconds:0.2f} сек.')
-    separation_line()
-
-
-def warnings(lines: list[str]) -> None:
-    """Вывести на экран предупреждения."""
-    for line in lines:
-        print(f'\t{line}')
-    print()
+    print_line()
+    print(f'Обработка заняла {seconds:0.2f} сек.')
